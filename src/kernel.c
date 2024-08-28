9e4e7fe8f526aab256d7c0a0622447763e575be2
Linus Honer <lhon2701@gmail.com>
2024-08-28 13:05:09 -0500

    Interrupt handling

--- src/kernel.c
+++ src/kernel.c
@@ -6,10 +6,12 @@ void kernel_main(void)
 {
 	uart_init();
 	init_printf(0, putc);
-	int el = get_el();
-	printf("Exception level: %d \r\n", el);
+	irq_vector_init();
+	timer_init();
+	enable_interrupt_controller();
+	enable_irq();
 
-	while (1) {
+	while (1){
 		uart_send(uart_recv());
-	}
+	}	
 }
